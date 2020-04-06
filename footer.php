e83e0f702ca78082541a3fb07da41599e29954ae
mirajapdoict <63222719+mirajapdoict@users.noreply.github.com>
2020-04-06 11:17:13 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+    <footer>
+      <div class="container">
+          <p>&copy; Department of ICT, Galachipa Upazilla</p>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/ajaxloder.js"></script> 
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+    <link href="css/main.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+  </head>
+  <body>
+
+  <header class="container d-print-none">
+      <ul class="menu">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="serviceadd.php">Add Service</a></li>
+        <li><a href="#">Show Data</a>
+          <ul class="dropdown-content">
+            <li><a href="showData.php?union=1">গলাচিপা</a></li>
+            <li><a href="showData.php?union=2">আমখোলা</a></li>
+            <li><a href="showData.php?union=3">গোলখালী</a></li>
+            <li><a href="showData.php?union=4">রতন্দী তালতলী</a></li>
+            <li><a href="showData.php?union=5">ডাকুয়া</a></li>
+            <li><a href="showData.php?union=6">পানপট্টী</a></li>
+			<li><a href="showData.php?union=7">চর কাজল</a></li>
+			<li><a href="showData.php?union=8">চর বিশ্বাস</a></li>
+			<li><a href="showData.php?union=9">গজালিয়া</a></li>
+			<li><a href="showData.php?union=10">বকুল্বাড়ীয়া</a></li>
+			<li><a href="showData.php?union=11">কলাগাছিয়া</a></li>
+			<li><a href="showData.php?union=12">চিকনিকান্দী</a></li>
+			
+			
+          </ul>
+        </li>
+      </ul>
+    </header>
